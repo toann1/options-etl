2eb47ec6bbb1138cd61e75810f562d4b9713567b
Toan Nguyen <toanknguyen1@gmail.com>
2022-03-26 22:15:49 +1100

    Add batch configuration

---
+++ src/main/java/com/toanknguyen/optionsetl/batch/config/BatchConfiguration.java
@@ -0,0 +1,24 @@
+package com.toanknguyen.optionsetl.batch.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+    private final Logger logger = LoggerFactory.getLogger(BatchConfiguration.class);
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public BatchConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+}
